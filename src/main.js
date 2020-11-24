e689d1c7f587106a0691948264970355c3f30c67
Brian DeVries <bdevries95@gmail.com>
2020-11-24 10:58:39 -0500

    add tailwind css to project

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import firebase from "firebase";
+import "./assets/tailwind.css";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBamw9FNWuU50jJmdlfyaTABhvDFcNM-pg",
