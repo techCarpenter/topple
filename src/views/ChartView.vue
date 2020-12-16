2616b174f86d606f77dc639b6bf60fcea4881669
Brian DeVries <bdevries95@gmail.com>
2020-12-15 22:43:40 -0500

    import plotly config file

--- src/views/ChartView.vue
+++ src/views/ChartView.vue
@@ -16,70 +16,13 @@
 <script>
 import AccountSidebar from "../components/AccountSidebar";
 import ReactiveChart from "../components/ReactiveChart.vue";
+import plotlyConfig from "../data";
 
 export default {
   name: "ChartView",
   data() {
     return {
-      chart: {
-        uuid: "1234",
-        traces: [
-          {
-            x: [1, 2, 3, 4],
-            y: [30, 15, 8, 4],
-            mode: "lines",
-            type: "scatter",
-            line: {
-              shape: "spline",
-              smoothing: 0.6,
-              // color: "#e34234",
-              width: 2
-            },
-            name: "Loan 1"
-          },
-          {
-            x: [1, 2, 3, 4],
-            y: [50, 25, 13, 7],
-            mode: "lines",
-            type: "scatter",
-            line: {
-              shape: "spline",
-              smoothing: 0.6,
-              // color: "#e34234",
-              width: 2
-            },
-            name: "Loan 2"
-          }
-        ],
-        layout: {
-          title: "Paydown Chart",
-          xaxis: {
-            title: "Month",
-            showgrid: true,
-            zeroline: true
-          },
-          yaxis: {
-            title: "Loan amount ($)",
-            showline: true
-          },
-          font: {
-            family: "Arial",
-            size: 14,
-            color: "#222222"
-          },
-          legend: {
-            orientation: "h",
-            x: 0.5,
-            y: 1.05,
-            yanchor: "middle",
-            xanchor: "center"
-          },
-          margin: {
-            pad: 0
-          }
-        },
-        config: { displaylogo: false, responsive: true }
-      }
+      chart: plotlyConfig
     };
   },
   components: {
@@ -97,12 +40,13 @@ export default {
 <style scoped>
 .chart-main-window {
   display: flex;
+  flex: 1 1 auto;
   flex-direction: column;
   align-items: center;
 }
 .chart-container {
   display: flex;
   flex-direction: row;
-  flex-basis: auto;
+  flex: 1;
 }
 </style>
