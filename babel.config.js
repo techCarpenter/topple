ffb99fc6d488d8318dc6c0b75030d2b0469ed809
Brian DeVries <bdevries95@gmail.com>
2021-07-10 22:20:54 -0400

    add 'transform-remove-console'

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
+const presets = ["@vue/cli-plugin-babel/preset"];
+const plugins = [];
+
+if (process.env["ENV"] === "prod") {
+  plugins.push("transform-remove-console");
+}
+
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  presets,
+  plugins
 };
